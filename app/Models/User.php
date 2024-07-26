c4e01584b50cb6bf011aa5d8ae8e9f05ebc8fa7a
Salvatore Boettcher <sal@prositemedia.com>
2024-07-25 22:20:23 -0400

    worked on crud

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasAvatar;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable
 {
@@ -45,4 +49,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+
 }
---
+++ resources/views/vendor/filament-icons/icon-column.blade.php
@@ -0,0 +1,5 @@
+<div>
+    @if($getState())
+        <x-filament-icon :icon="$getState()" size="h-6 w-6" :style="'text-2xl'" />
+    @endif
+</div>
